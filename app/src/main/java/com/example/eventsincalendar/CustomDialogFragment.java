0dec876bf5d3556476a557b9687f5b62e90fd732
SVo7777777 <svetl77voronina@yandex.ru>
2025-08-09 15:09:21 +0300

    Commit from 04.08.2025

---
+++ app/src/main/java/com/example/eventsincalendar/CustomDialogFragment.java
@@ -0,0 +1,27 @@
+package com.example.eventsincalendar;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class CustomDialogFragment extends DialogFragment {
+
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        assert getArguments() != null;
+        String attention = getArguments().getString("attention");
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        return builder
+                .setTitle("ВНИМАНИЕ!")
+                .setMessage(attention)
+                .setPositiveButton("OK", null)
+                .setNegativeButton("",null)
+
+
+
+                .create();
+    }
+}
